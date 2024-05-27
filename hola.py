034d42eceae7c770924dba1a2685fee668bfbfb4
Francisco Anllo <165914589+lolxdahre@users.noreply.github.com>
2024-05-27 11:26:10 -0300

    Update hola.py

--- hola.py
+++ hola.py
@@ -64,11 +64,120 @@ def saldo (s:list[(chr,int)]) -> int:
             i = i-y
     return i
 
+
 def vocales (s:str) -> bool:
     cantidad:int = 0
-    n:list = ['a','e','i','o','u','A','E','I','O','U']
-    for i in s:
-        if pertenece (n,i) == True:
+    n:list[str] = ['a','e','i','o','u','A','E','I','O','U']
+    for char in s:
+        if pertenece (n,char):
             cantidad += 1
-            n = n.remove(i)
+            n.remove(char)
     return cantidad>=3
+
+def pertenece (s:list, e) -> bool:
+    i:int = 0
+    while i < len (s) and e != s[i]:
+        i+=1
+    
+    return i < len (s)
+
+def borrar_pares (s:list[float]) -> None:
+    for i in range (0,len(s),2):
+        s[i]=0
+
+def borrar_pares_nuevo (s:list[float]) -> list[float]:
+    n:list[float] = []
+    for i in range (0,len(s)):
+        if i%2 == 0:
+            s[i]=0
+            n.append(s[i])
+        else:
+            n.append(s[i])
+    return n
+
+print (borrar_pares_nuevo([1,2,3,4]))
+
+def borrar_vocales (s:list[chr]) -> str:
+    n:list = ['a','e','i','o','u','A','E','I','O','U']
+    for char in s:
+        if pertenece (n,char):
+            s.remove(char)
+    return s
+
+print (borrar_vocales(['h','o','l','a']))
+
+def reemplaza_vocales (s:list[chr]) -> list [chr]:
+    n:list = ['a','e','i','o','u','A','E','I','O','U']
+    for i in range(0,len(s)):
+        if pertenece (n,s[i]):
+            s[i]='-'
+    return s
+
+print (reemplaza_vocales(['a','b','c','a','i','r']))
+
+def da_vuelta_str (s:list[chr]) -> list[chr]:
+    n:list[chr] = []
+    for i in range(len(s)-1,-1,-1):
+        n.append(s[i])
+    return n 
+
+print (da_vuelta_str(['h','o','l','a']))
+
+def eliminar_repetidos (s:list[chr]) -> list[chr]:
+    for char in s:
+        if s.count(char) > 1:
+            s.remove(char)
+    return s
+
+## OTRA FORMA SIN REMOVE
+def eliminar_repetidos_2 (s:list[chr]) -> list[chr]:
+    n:list[chr]=[]
+    for char in s:
+        if not (pertenece (n,char)):
+            n.append(char)
+    return n
+
+print (eliminar_repetidos_2 (['h','h','h','o','h','l','a','a']))
+
+def aprobado (s:list[int]) -> int:
+    res:int = None
+    if promedio (s) >= 7 and not(menor_a_4(s)):
+        res = 1
+    elif not (menor_a_4 (s)):
+        res = 2
+    else:
+        res = 3
+    return res
+
+def promedio (s:list[int]) -> int:
+    prom:int = 0
+    for nota in s:
+        prom += nota
+    return prom/len(s)
+
+def menor_a_4 (s:list[int]) -> bool:
+    res:bool = False
+    for i in range(0,len(s)):
+        if s[i]<4:
+            res = True
+    return res
+
+def nombres () -> list[str]:
+    n:list[str] = []
+    s = input ("hola")
+    while s != "listo":
+        n.append(s)
+        s = input("chau")
+    return n
+
+def pertenece_a_cada_uno (s:list[list[int]],e:int,res:list[bool]) -> None:
+    for n in range(0,len(s)):
+        if pertenece (s[n],e):
+            res.append(True)
+        else:
+            res.append(False)
+
+res = []
+pertenece_a_cada_uno ([[1,2],[3,4],[2,3],[5,6],[3,8]],3, res)
+print(res)
+
