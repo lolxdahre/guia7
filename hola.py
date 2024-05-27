6e8d7dd2ca91e94cdd6c42db5603386bb1230d87
Francisco Anllo <165914589+lolxdahre@users.noreply.github.com>
2024-05-27 07:08:19 -0300

    Update hola.py

--- hola.py
+++ hola.py
@@ -39,4 +39,36 @@ def palabra_larga (s:list[str]) -> bool:
 ##ejercicio 6
 def palindromo (s:str) -> bool:
     i:int = 0
+    if len (s)%2 == 0:
+        while s[i] == s[len(s)-1-i] and len (s)/2 >= i:
+            i+=1
+        return i==(len(s)/2)+1
+    else: 
+        while s[i] == s[len(s)-1-i] and (len (s)+1)/2 > i:
+            i+=1
+        return i== (len(s)+1)/2
     
+##def contraseña (s:str) -> str:
+    ##if len (s) < 5:
+    ##    return "ROJA"
+    ##elif len (s) > 8:
+   ## else: 
+     ##   return "AMARILLA"
+
+def saldo (s:list[(chr,int)]) -> int:
+    i:int = 0
+    for (x,y) in s:
+        if x == 'I':
+            i += y
+        if x == 'R':
+            i = i-y
+    return i
+
+def vocales (s:str) -> bool:
+    cantidad:int = 0
+    n:list = ['a','e','i','o','u','A','E','I','O','U']
+    for i in s:
+        if pertenece (n,i) == True:
+            cantidad += 1
+            n = n.remove(i)
+    return cantidad>=3
